e03cbf46f4da9e50727662b72f7f24b06970b897
Shreyas707Makwana <shreyasmakwana.smh@gmail.com>
2025-04-01 13:46:07 +0530

    worked on app.js and app.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import UrlShortener from './components/UrlShortener';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 className="heading">URL Shortener</h1>
+        <p className="subtitle">Paste a long URL and get a short link that never expires</p>
+        <UrlShortener />
+      </div>
     </div>
   );
 }
